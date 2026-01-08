07d5bf133543c114ffbceeee03f387a980933f59
chalimbu <sebitaszapata12@gmail.com>
2026-01-08 14:43:14 -0500

    first part of the exercise using value state

--- src/main/java/spendreport/FraudDetector.java
+++ src/main/java/spendreport/FraudDetector.java
@@ -18,6 +18,11 @@
 
 package spendreport;
 
+import org.apache.flink.api.common.functions.OpenContext;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeInfo;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
 import org.apache.flink.walkthrough.common.entity.Alert;
@@ -30,6 +35,8 @@ public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert
 
 	private static final long serialVersionUID = 1L;
 
+	private ValueState<Boolean> lastTransactionSmall;
+
 	private static final double SMALL_AMOUNT = 1.00;
 	private static final double LARGE_AMOUNT = 500.00;
 	private static final long ONE_MINUTE = 60 * 1000;
@@ -40,9 +47,23 @@ public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert
 			Context context,
 			Collector<Alert> collector) throws Exception {
 
-		Alert alert = new Alert();
-		alert.setId(transaction.getAccountId());
+		if(lastTransactionSmall.value() != null && lastTransactionSmall.value() &&transaction.getAmount()>LARGE_AMOUNT){
+			Alert alert = new Alert();
+			alert.setId(transaction.getAccountId());
 
-		collector.collect(alert);
+			collector.collect(alert);
+		}
+		if(transaction.getAmount()<SMALL_AMOUNT){
+			lastTransactionSmall.update(true);
+		}else{
+			lastTransactionSmall.update(false);
+		}
+
+	}
+
+	@Override
+	public void open(OpenContext openContext) throws Exception {
+		ValueStateDescriptor ved= new ValueStateDescriptor<>("lastTransactionSmall", Types.BOOLEAN);
+		lastTransactionSmall = getRuntimeContext().getState(ved);
 	}
 }
